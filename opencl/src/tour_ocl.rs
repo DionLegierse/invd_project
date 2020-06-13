73cec8c0093d54d9dfb1a8fa748ac8c08dc58cf9
Dion Legierse <djlegierse@hotmail.com>
2020-06-13 11:45:48 +0200

    workin for 1 workgroup and 1 solution

--- opencl/src/tour_ocl.rs
+++ opencl/src/tour_ocl.rs
@@ -17,34 +17,30 @@ fn print_ocl_board(board : &Vec<i8>){
 
 pub fn knights_tour_opencl(size : usize) -> ocl::Result<()> {
 
-    let ocl_function = std::fs::read_to_string("src/tour.ocl").expect("Error opening file!\n");
+    let ocl_functions_raw = std::fs::read_to_string("src/ocl/tour.ocl").expect("Error opening file!\n");
+
+    let ocl_functions = ("__constant char size = ".to_string()) + 
+                        (&(size as i8).to_string()) +
+                        (&";\n".to_string()) + 
+                        &ocl_functions_raw.to_string();
 
     let pro_que = ProQue::builder()
-        .src(ocl_function.to_string())
-        .dims(size.pow(2))
+        .src(ocl_functions)
+        .dims(1)
         .build()?;
 
-    let board = pro_que.create_buffer::<i8>()?;
-
-    let char_size = size as i8;
+    let result_list = pro_que.create_buffer::<i8>()?;
 
     let kernel_tour = pro_que.kernel_builder("knights_tour_solve")
-        .arg(&board)
-        .arg(12i8)
-        .arg(&char_size)
+        .arg(&result_list)
         .build()?;
-
-
 
     unsafe{ 
         kernel_tour.enq()?;
     }
 
-
-  
-
-    let mut result = vec![0i8; board.len()];
-    board.read(&mut result).enq()?;
+    let mut result = vec![0i8; result_list.len()];
+    result_list.read(&mut result).enq()?;
 
     print_ocl_board(&result);
 
