9b7e435b8f9249cf97397f2936cfb26a492ebd47
Dion Legierse <djlegierse@hotmail.com>
2020-06-16 10:06:28 +0200

    It is slower now????

--- opencl/src/tour_ocl.rs
+++ opencl/src/tour_ocl.rs
@@ -20,9 +20,11 @@ pub fn knights_tour_opencl(size : usize, start : usize) -> ocl::Result<Vec<i8>> 
         .build()?;
 
     let result_list : Buffer<i8> = Buffer::builder().len(size.pow(2) * 8).queue(pro_que.queue().clone()).build()?;
+    let is_running : Buffer<u8> = Buffer::builder().len(1).queue(pro_que.queue().clone()).fill_val(1u8).build()?;
 
     let kernel_tour = pro_que.kernel_builder("knights_tour_solve")
         .arg(&result_list)
+        .arg(&is_running)
         .arg(&(start as i8))
         .build()?;
 
@@ -30,8 +32,20 @@ pub fn knights_tour_opencl(size : usize, start : usize) -> ocl::Result<Vec<i8>> 
         kernel_tour.enq()?;
     }
 
-    let mut result = vec![0i8; result_list.len()];
-    result_list.read(&mut result).enq()?;
+    let mut results = vec![0i8; result_list.len()];
+    let mut result = vec![0i8; size.pow(2)];
+
+    result_list.read(&mut results).enq()?;
+
+    for n in 0..8 {
+        let start = size.pow(2) * n;
+        let end = start + size.pow(2);
+        let slice = &results[start..end];
+
+        if !slice.contains(&0i8) {
+            result = slice.to_vec();
+        }
+    }
 
     Ok(result)
 }
